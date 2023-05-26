daaf10f7f7c11606c3fa5438aea7be02b5db9137
lokesh-ns <lokeshns7476loki@gmail.com>
2023-05-26 17:46:34 +0530

    allowing hosts

--- Portfolio_Project/settings.py
+++ Portfolio_Project/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-8wh%nmc@xk%0tw!+p&r7*-)bsk^e$ao3*++%^3_^5+r+oj4(g5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
